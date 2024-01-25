f61605c019c73033a8b65ca670e250367003e6d8
TS-SAHANA <115524513+TS-SAHANA@users.noreply.github.com>
2024-01-25 21:21:43 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://sahanats4:Sanju%401702@cluster0.eeijcob.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
